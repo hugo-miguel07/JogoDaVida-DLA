26cd341283482166dd21265e5ee0f2db5eb20ef9
hugo-miguel07 <hugomiguel.cacao@gmail.com>
2025-10-19 23:41:20 +0100

    rule do jogoDaVida

--- src/JogoDaVidaProcessing.java
+++ src/JogoDaVidaProcessing.java
@@ -74,7 +74,7 @@ public class JogoDaVidaProcessing implements IProcessingApp {
 		}
 		else if (parent.key == 'b' || parent.key == 'B')
 		{
-			automata.setRule(true);
+			automata.setRule(!automata.getRule());
 		}
 	}
 
